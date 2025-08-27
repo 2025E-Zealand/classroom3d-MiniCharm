14643a0a262b3c56e5fbf80a77e17998e86a9940
Johanne Marie Dehn Nielsen <jon010@edu.zealand.dk>
2025-08-27 11:43:27 +0200

    Added class Studerende og properties Navn, Fødselsmåned, fødselsdag

---
+++ ClassRoomNet60/Studerende.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    public class Studerende
+    {
+
+        public string Navn { get; set; }
+        public int FødselsdagsMåned { get; set; }
+        public int Fødselsdag { get; set; }
+
+        public Studerende(string navn, int fødselsMåned, int fødselsDag)
+        {
+            Navn = navn;
+            FødselsdagsMåned = fødselsMåned;
+            Fødselsdag = fødselsDag;
+        }
+
+    }
+}
